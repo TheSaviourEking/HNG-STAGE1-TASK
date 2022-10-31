a5705431f07245b2ccf49cb75a932209cc7bd10a
TheSaviourEking <saviour.eking@outlook.com>
2022-10-31 05:06:11 +0100

    add new updates for Heroku deploy

--- GetApi/settings.py
+++ GetApi/settings.py
@@ -117,13 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = "static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-from django import django_heroku
-django_heroku.settings(locals())
